a0cd2d1db1451559d79dc38cd9a6fad07572b689
Shayma <shaymaab1424@gmail.com>
2026-05-29 04:21:01 +0300

    Add task management features and improve UI layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,72 @@
+import { useState } from "react";
+
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() {
+    if (task.trim() === "") return;
+
+    const newTask = {
+      text: task,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setTask("");
+  }
+
+  function deleteTask(indexToDelete) {
+    const updatedTasks = tasks.filter((_, index) => index !== indexToDelete);
+    setTasks(updatedTasks);
+  }
+  function toggleComplete(indexToToggle) {
+    const updatedTasks = tasks.map((task, index) => {
+      if (index === indexToToggle) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+  }
   return (
     <div className="container">
       <h1>Todo App</h1>
 
       <div>
-        <input type="text" placeholder="Enter a task" />
-        <button>Add</button>
-      </div>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
 
-      <ul>
-        <li>Study React</li>
-        <li>Finish project</li>
-      </ul>
+        <button onClick={addTask}>Add</button>
+        {tasks.length === 0 && <p>No tasks yet 🚀</p>}
+        <ul>
+          {tasks.map((item, index) => (
+            <li key={index}>
+              <span
+                style={{
+                  textDecoration: item.completed ? "line-through" : "none",
+                }}
+              >
+                {item.text}
+              </span>
+
+              <button onClick={() => toggleComplete(index)}>
+                Done
+              </button>
+
+              <button onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </div>
   );
 }
